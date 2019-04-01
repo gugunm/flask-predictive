a269e56f7bb45bf71309cf1a0b73a748da6bfd58
elianalien <elian.alien19@gmail.com>
2019-04-01 18:07:26 +0700

    test upload download test function

--- uploader_test.py
+++ uploader_test.py
@@ -1,8 +1,9 @@
 import gcs_module as gcs 
+import datetime
 
 if __name__ == '__main__':
     # upload aicollective model (aicollective.pkl) to bucket and rename it as model1.pkl
-    gcs.upload_blob('analytics.arkana.ai', './models/aicollective.pkl', 'model1.pkl')
+    gcs.upload_blob('analytics.arkana.ai', './models/aicollective.pkl', 'savedmodel/{}.pkl'.format(datetime.datetime.now()))
 
     # download model1.pkl from bucket and download as testing_download.pkl in models folder
-    gcs.download_blob('analytics.arkana.ai', 'model1.pkl', 'models/testing_download.pkl')
+    gcs.download_blob('analytics.arkana.ai', 'model1.pkl', 'models/{}_dwnloaded_model.pkl'.format(datetime.datetime.now()))
